073f995db592595f47a6af4f5d7ac8ae0c7b3d5d
MrMori-Dev <148895434+MrMori-Dev@users.noreply.github.com>
2024-06-30 15:48:16 +0330

    Delete 07-Database directory

--- 07-Database/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$userName = 'root';
-$password = '';
-
-$dsn = 'mysql:host=localhost;dbname=users';
-$conn = new PDO($dsn, $userName, $password);
-$sql = 'SELECT * FROM `client_statistics` ORDER BY `name`,`email`';
