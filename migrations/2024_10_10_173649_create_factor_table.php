a1dabe49916e5a4a031da2d95d473691b41adba1
MrMori-Dev <148895434+MrMori-Dev@users.noreply.github.com>
2024-10-13 20:29:57 +0330

    add new migration task in laravel

---
+++ migrations/2024_10_10_173649_create_factor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factor', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_id');
+            $table->string('totalPrice');
+            $table->string('discount');
+            $table->string('discountCode_id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factor');
+    }
+};
---
+++ migrations/2024_10_10_173718_create_discount_code_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_code', function (Blueprint $table) {
+            $table->id();
+            $table->string('discount');
+            $table->string('user_id');
+            $table->string('details');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_code');
+    }
+};
